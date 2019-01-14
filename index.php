a4497eaaf1aaf8d767ae99b142c05c8ac22337d7
denisitra <den.bogachev.1999@mail.ru>
2019-01-15 02:53:07 +0300

    mb

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
- 
+<?php
+require_once('ArrayChecking.php');
+$PACKAGES = [
+    'A' => [
+        'name' => 'A',
+        'dependencies' => ['B', 'C'],
+    ],
+    'B' => [
+        'name' => 'B',
+        'dependencies' => [],
+    ],
+    'C' => [
+        'name' => 'C',
+        'dependencies' => ['B', 'D'],
+    ],
+    'D' => [
+        'name' => 'D',
+        'dependencies' => [],
+    ]
+];
+try {
+    $entity = new ArrayChecking();
+    var_dump($entity->getAllPackageDependencies($PACKAGES, 'A'));
+} catch (DependencyExclusion $e) {
+    echo $e->getMessage();
+} 
